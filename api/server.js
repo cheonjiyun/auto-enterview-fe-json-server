feede36e8d05e6581e4bbb5885cc3397eb8cf068
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-09-02 14:17:03 +0900

    cors

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
 
-server.use((req, rs, next) => {
+server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://auto-enterview-fe.vercel.app/");
     res.header("Access-Control-Allow-Credentials", true);
     next();
